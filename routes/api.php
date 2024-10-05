58f0c395585bccc7c69a02ad8fb2cd08cd8519d2
Moji <472306m@gmail.com>
2024-10-05 11:55:21 +0330

    middleware for routes(only authed user can see posts or  do other things)

--- routes/api.php
+++ routes/api.php
@@ -9,15 +9,19 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::post('/register',[UserController::class,'store']);
-Route::post('/login',[UserController::class,'login']);
+Route::post('/login',[UserController::class,'login'])->name('login');
+Route::post('logout',[UserController::class,'logout'])->middleware('auth:sanctum');
 
-Route::resource('posts',PostController::class);
-Route::get('posts/{post}/comments',[PostController::class,'comments']);
+Route::group(['middleware'=>'auth:sanctum'],function(){
+
+    Route::resource('posts',PostController::class);
+    Route::get('posts/{post}/comments',[PostController::class,'comments']);
+    Route::resource('categories',CategoryController::class);
+    Route::get('categories/{category}/posts',[CategoryController::class,'posts']);
+    Route::get('categories/{category}/comments',[CategoryController::class,'comments']);
+
+    Route::post('comments',[CommentController::class,'store']);
+    Route::delete('comments/{comment}',[CommentController::class,'delete']);
+});
 
 
-Route::resource('categories',CategoryController::class);
-Route::get('categories/{category}/posts',[CategoryController::class,'posts']);
-Route::get('categories/{category}/comments',[CategoryController::class,'comments']);
-
-Route::post('comments',[CommentController::class,'store']);
-Route::delete('comments/{comment}',[CommentController::class,'delete']);
